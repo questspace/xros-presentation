5400fe652e2e6c5ade374b2c82a100b9b403bd50
Michal Takáč <takacmichal92@gmail.com>
2024-09-21 17:39:21 +0200

    feat: prepare for hosting on github pages

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
             Taking inspiration from Dynamicland
           </div>
           <iframe
-            className="w-[1510px] h-[950px] m-auto mt-0"
+            className="w-3/5 h-[660px] m-auto mt-0"
             src="https://dynamicland.org/2024/Intro/"
           />
         </section>
@@ -79,7 +79,7 @@ function App() {
           <div className="font-bold text-7xl mb-10">
             Taking inspiration from Dynamicland
           </div>
-          <div className="w-full m-auto">
+          <div className="w-4/5 m-auto">
             <img src="/images/move-to-zoom-signal.jpg" className="w-full" />
           </div>
         </section>
@@ -88,7 +88,7 @@ function App() {
           <div className="font-bold text-7xl mb-10">
             Taking inspiration from Dynamicland
           </div>
-          <div className="w-full flex flex-row justify-between gap-x-4">
+          <div className="w-4/5 m-auto">
             <img
               src="/images/making-music-dynamicland.jpg"
               className="w-full"
@@ -101,14 +101,14 @@ function App() {
             QuestSpace - Communal Hackerspace...
           </div>
           <div className="font-bold text-3xl mb-10">...with a (XR) twist</div>
-          <div className="w-3/4 m-auto">
+          <div className="w-2/3 m-auto">
             <img src="/images/communal-science-lab.png" className="w-full" />
           </div>
         </section>
 
         <section className="p-10">
           <div className="font-bold text-7xl mb-32">QuestSpace XR + AI</div>
-          <div className="font-bold text-5xl max-w-[1600px] mx-auto">
+          <div className="font-bold text-4xl max-w-[1600px] mx-auto">
             <ul className="leading-loose">
               <li>
                 Physical objects recognizable through headset / smartphone
@@ -141,8 +141,8 @@ function App() {
           <div className="font-bold text-3xl mb-10">
             At first, focus on building fun experiments in WebXR!
           </div>
-          <div className="w-2/3 m-auto p-10 -mt-10">
-            <div className="w-full flex flex-row justify-between gap-x-4">
+          <div className="w-1/2 m-auto p-10 -mt-10">
+            <div className="w-full flex flex-row justify-between gap-x-4 gap-y-2 -mb-6">
               <img src="/images/aframe-logo.png" className="w-1/3" />
               <img src="/images/webxr-threejs.jpg" className="w-2/3" />
             </div>
@@ -164,7 +164,7 @@ function App() {
             <br />❌ We cannot use video feed from Meta Quest for Computer
             Vision / AI hacking
           </div>
-          <div className="w-3/4 m-auto">
+          <div className="w-2/3 m-auto">
             <img
               src="/images/meta-quest-3-passthrough.png"
               className="w-full"
@@ -185,7 +185,7 @@ function App() {
               limitations of existing systems.&quot; &mdash;Leap Motion
             </blockquote>
           </div>
-          <div className="w-3/4 m-auto">
+          <div className="w-2/3 m-auto">
             <img src="/images/northstar-person.jpg" className="w-full" />
           </div>
         </section>
@@ -204,7 +204,7 @@ function App() {
             AI hacking!
           </div>
           <iframe
-            className="w-[1280px] h-[768px] m-auto"
+            className="w-1/2 h-[520px] m-auto -mt-10"
             src="https://www.youtube-nocookie.com/embed/7m6J8W6Ib4w?si=TycC2gdPwxesIlOr&autoplay=1"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -218,7 +218,7 @@ function App() {
           <div className="font-bold text-3xl mb-10 w-3/4 mx-auto">
             Great community project to build the headsets together
           </div>
-          <div className="w-3/4 m-auto">
+          <div className="w-2/3 m-auto">
             <img src="/images/northstar-electronics.jpg" className="w-full" />
           </div>
         </section>
@@ -246,7 +246,7 @@ function App() {
             for a specific class of problems
           </div>
           <iframe
-            className="w-[1280px] h-[768px] m-auto"
+            className="w-2/3 h-[680px] m-auto -mt-10"
             src="https://www.youtube-nocookie.com/embed/R81egpgDzbY?autoplay=1"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -263,14 +263,14 @@ function App() {
             Writing XR OS hackable to the &quot;atomic&quot; core? Smalltalk
             (Pharo)
           </div>
-          <div className="w-4/5 mx-auto">
+          <div className="w-2/3 mx-auto">
             <img src="/images/pharo.png" className="w-full" />
           </div>
         </section>
 
         <section className="p-10">
           <div className="font-bold text-7xl mb-32">QuestSpace community</div>
-          <div className="font-bold text-5xl">
+          <div className="font-bold text-4xl">
             <ul className="leading-loose">
               <li>We need to fill the space with creative, crazy people</li>
               <li>Dreamers and Makers: one group helps the other</li>
